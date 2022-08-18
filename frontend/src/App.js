63ec8aa29831d897defa7bab7dd9822e93f10cb3
edbosisto <e.bosisto@gmail.com>
2022-08-18 15:12:55 +0200

    created function based components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Header from './components/header';
+import NotesListPage from './pages/noteslistpage';
 
 function App() {
   return (
     <div className="App">
-      TEST
+      <Header />
+      <NotesListPage />
     </div>
   );
 }
