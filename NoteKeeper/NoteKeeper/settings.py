f94cf187f143c957e0dd4c46c23fc03385285978
edbosisto <e.bosisto@gmail.com>
2022-08-18 15:31:58 +0200

    corsheaders installed and added to settings.py

--- NoteKeeper/NoteKeeper/settings.py
+++ NoteKeeper/NoteKeeper/settings.py
@@ -39,16 +39,24 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api.apps.ApiConfig',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    "corsheaders.middleware.CorsMiddleware",
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000/",
 ]
 
 ROOT_URLCONF = 'NoteKeeper.urls'
